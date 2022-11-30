07aefc1618a1c8408219da892ba5649d69d84f73
priya-bangaru <bj960863@gmail.com>
2022-11-30 16:52:04 +0530

    30/11/22

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import ClassComponent from'./ClassComponent'
+import PropsExample from './PropsExample'
+function App(){
+  return(
+    <section>
+    <PropsExample name="sai" age="19"/>
+    <PropsExample name="sravs"age="19"/>
+   { /*<p>Good afternoon</p>
+    <h1>heading tag</h1>
+    <img src="https://www.epw.in/system/files/Pawan_Kalyan_Featured.jpg"width="40%"alt="pawankalyan"/>
+    <ClassComponent/> */}  
+   </section>
+    )
+}
+export default App
